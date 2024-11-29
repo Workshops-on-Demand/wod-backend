ebcbde5d214f0c8bc19546ec82f18bbb560a71d0
Bruno Cornec <bruno@victoria.frmug.org>
2024-11-29 14:53:49 +0100

    pkeys added with alterw!

--- install/install-system.sh
+++ install/install-system.sh
@@ -254,7 +254,7 @@ EOF
 	psql --dbname=$POSTGRES_DB --username=postgres --host=localhost -c "UPDATE users set password=crypt('$WODAPIDBADMINPWD',gen_salt('bf')) where username='$WODAPIDBADMIN';"
 	echo "Setup user_roles table not done elsewhere"
 	psql --dbname=$POSTGRES_DB --username=postgres --host=localhost -c 'CREATE TABLE IF NOT EXISTS user_roles ("createdAt" timestamp DEFAULT current_timestamp, "updatedAt" timestamp DEFAULT current_timestamp, "roleId" integer CONSTRAINT no_null NOT NULL REFERENCES roles (id), "userId" integer CONSTRAINT no_null NOT NULL PRIMARY KEY REFERENCES users (id));'
-	psql --dbname=$POSTGRES_DB --username=postgres --host=localhost -c 'ALTER TABLE ADD CONSTRAINT "ID_PKEY" PRIMARY KEY REFERENCES users (id), PRIMARY KEY (roleId,userId));'
+	psql --dbname=$POSTGRES_DB --username=postgres --host=localhost -c 'ALTER TABLE user_roles ADD CONSTRAINT "ID_PKEY" PRIMARY KEY REFERENCES users (id), PRIMARY KEY (roleId,userId));'
 	# Get info on roles and users already declared
 	userroleid=`psql --dbname=$POSTGRES_DB --username=postgres --host=localhost -AXqtc "SELECT id FROM roles WHERE name='user';"`
 	moderatorroleid=`psql --dbname=$POSTGRES_DB --username=postgres --host=localhost -AXqtc "SELECT id FROM roles WHERE name='moderator';"`
