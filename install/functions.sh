333c11b9e0e693b8baf70282e236c5446cfa26be
Bruno Cornec <bruno@victoria.frmug.org>
2024-11-29 13:48:40 +0100

    rename WODAPIDBPWD into WODAPIDBUSERPWD and introduce WODAPIDBADMIN/WODAPIDBADMINPWD

--- install/functions.sh
+++ install/functions.sh
@@ -15,9 +15,17 @@ if [ -f "$ANSIBLEDIR/group_vars/$PBKDIR" ]; then
 	if [ _"$WODAPIDBUSER" = _"null" ]; then
 		WODAPIDBUSER=""
 	fi
-	WODAPIDBPWD=`cat "$ANSIBLEDIR/group_vars/$PBKDIR" | yq '.WODAPIDBPWD' | sed 's/"//g'`
-	if [ _"$WODAPIDBPWD" = _"null" ]; then
-		WODAPIDBPWD=""
+	WODAPIDBADMIN=`cat "$ANSIBLEDIR/group_vars/$PBKDIR" | yq '.WODAPIDBADMIN' | sed 's/"//g'`
+	if [ _"$WODAPIDBADMIN" = _"null" ]; then
+		WODAPIDBADMIN=""
+	fi
+	WODAPIDBUSERPWD=`cat "$ANSIBLEDIR/group_vars/$PBKDIR" | yq '.WODAPIDBUSERPWD' | sed 's/"//g'`
+	if [ _"$WODAPIDBUSERPWD" = _"null" ]; then
+		WODAPIDBUSERPWD=""
+	fi
+	WODAPIDBADMINPWD=`cat "$ANSIBLEDIR/group_vars/$PBKDIR" | yq '.WODAPIDBPADMINWD' | sed 's/"//g'`
+	if [ _"$WODAPIDBADMINPWD" = _"null" ]; then
+		WODAPIDBADMINPWD=""
 	fi
 fi
 if [ -f "$ANSIBLEPRIVDIR/group_vars/$PBKDIR" ]; then
@@ -25,17 +33,31 @@ if [ -f "$ANSIBLEPRIVDIR/group_vars/$PBKDIR" ]; then
 	if [ _"$WODAPIDBUSER2" = _"null" ]; then
 		WODAPIDBUSER2=""
 	fi
-	WODAPIDBPWD2=`cat "$ANSIBLEPRIVDIR/group_vars/$PBKDIR" | yq '.WODAPIDBPWD' | sed 's/"//g'`
-	if [ _"$WODAPIDBPWD2" = _"null" ]; then
-		WODAPIDBPWD2=""
+	WODAPIDBADMIN2=`cat "$ANSIBLEPRIVDIR/group_vars/$PBKDIR" | yq '.WODAPIDBADMIN' | sed 's/"//g'`
+	if [ _"$WODAPIDBADMIN2" = _"null" ]; then
+		WODAPIDBADMIN2=""
+	fi
+	WODAPIDBUSERPWD2=`cat "$ANSIBLEPRIVDIR/group_vars/$PBKDIR" | yq '.WODAPIDBUSERPWD' | sed 's/"//g'`
+	if [ _"$WODAPIDBUSERPWD2" = _"null" ]; then
+		WODAPIDBUSERPWD2=""
+	fi
+	WODAPIDBADMINPWD2=`cat "$ANSIBLEPRIVDIR/group_vars/$PBKDIR" | yq '.WODAPIDBADMINPWD' | sed 's/"//g'`
+	if [ _"$WODAPIDBADMINPWD2" = _"null" ]; then
+		WODAPIDBADMINPWD2=""
 	fi
 fi
 # Overload standard with private if anything declared there
 if [ _"$WODAPIDBUSER2" != _"" ]; then
 	WODAPIDBUSER=$WODAPIDBUSER2
 fi
-if [ _"$WODAPIDBPWD2" != _"" ]; then
-	WODAPIDBPWD=$WODAPIDBPWD2
+if [ _"$WODAPIDBUSERPWD2" != _"" ]; then
+	WODAPIDBUSERPWD=$WODAPIDBUSERPWD2
+fi
+if [ _"$WODAPIDBADMIN2" != _"" ]; then
+	WODAPIDBADMIN=$WODAPIDBADMIN2
+fi
+if [ _"$WODAPIDBADMINPWD2" != _"" ]; then
+	WODAPIDBADMINPWD=$WODAPIDBADMINPWD2
 fi
 
 if [ _"$WODAPIDBUSER" = _"" ]; then
@@ -43,12 +65,24 @@ if [ _"$WODAPIDBUSER" = _"" ]; then
 	WODAPIDBUSER="moderator"
 	echo "Using default $WODAPIDBUSER instead"
 fi
-if [ _"$WODAPIDBPWD" = _"" ]; then
-	echo "You need to configure WODAPIDBPWD in your $PBKDIR ansible variable file"
-	WODAPIDBPWD="UnMotDePassCompliqué"
-	echo "Using default $WODAPIDBPWD instead"
+if [ _"$WODAPIDBUSERPWD" = _"" ]; then
+	echo "You need to configure WODAPIDBUSERPWD in your $PBKDIR ansible variable file"
+	WODAPIDBUSERPWD="UnMotDePassCompliqué"
+	echo "Using default $WODAPIDBUSERPWD instead"
+fi
+if [ _"$WODAPIDBADMIN" = _"" ]; then
+	echo "You need to configure WODAPIDBADMIN in your $PBKDIR ansible variable file"
+	WODAPIDBADMIN="hackshack"
+	echo "Using default $WODAPIDBADMIN instead"
+fi
+if [ _"$WODAPIDBADMINPWD" = _"" ]; then
+	echo "You need to configure WODAPIDBADMINPWD in your $PBKDIR ansible variable file"
+	WODAPIDBADMINPWD="UnAutreMotDePassCompliqué"
+	echo "Using default $WODAPIDBADMINPWD instead"
 fi
 export WODAPIDBUSER
-export WODAPIDBPWD
+export WODAPIDBUSERPWD
+export WODAPIDBADMIN
+export WODAPIDBADMINPWD
 }
 
